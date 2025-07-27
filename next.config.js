28bc177bf7392a0d91bbb90453d6b1f44684b9a3
JONNYDAN <tuannobi44@gmail.com>
2025-07-27 21:52:38 +0700

    updated

--- next.config.js
+++ next.config.js
@@ -10,12 +10,31 @@ const nextConfig = {
       {
         source: '/:path*',
         headers: [
-          { 
+          {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' cdn.sanity.io *.facebook.com *.vercel.app" 
+            value: [
+              // Các directive cơ bản
+              "default-src 'self'",
+              
+              // Cho phép các script cần thiết
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.facebook.com *.vercel.app chimpstatic.com embed.tawk.to",
+              "script-src-elem 'self' 'unsafe-inline' *.facebook.com *.vercel.app chimpstatic.com embed.tawk.to",
+              
+              // Cho phép các style
+              "style-src 'self' 'unsafe-inline' fonts.googleapis.com",
+              
+              // Cho phép các hình ảnh
+              "img-src 'self' data: blob: cdn.sanity.io *.facebook.com *.fbcdn.net chimpstatic.com tawk.to",
+              
+              // Cho phép kết nối
+              "connect-src 'self' *.sanity.io *.facebook.com *.vercel.app chimpstatic.com embed.tawk.to",
+              
+              // Cho phép font
+              "font-src 'self' fonts.gstatic.com"
+            ].join('; ')
           }
         ],
-      }
+      },
     ];
   }
 }
