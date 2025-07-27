9f474050e6a3f125c1b9d7e147e946b6a05ddd8f
JONNYDAN <tuannobi44@gmail.com>
2025-07-27 22:02:35 +0700

    updated

--- next.config.js
+++ next.config.js
@@ -5,38 +5,22 @@ const nextConfig = {
   images: {
     domains: ['cdn.sanity.io'],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: [
-              // Các directive cơ bản
-              "default-src 'self'",
-              
-              // Cho phép các script cần thiết
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.facebook.com *.vercel.app chimpstatic.com embed.tawk.to",
-              "script-src-elem 'self' 'unsafe-inline' *.facebook.com *.vercel.app chimpstatic.com embed.tawk.to",
-              
-              // Cho phép các style
-              "style-src 'self' 'unsafe-inline' fonts.googleapis.com",
-              
-              // Cho phép các hình ảnh
-              "img-src 'self' data: blob: cdn.sanity.io *.facebook.com *.fbcdn.net chimpstatic.com tawk.to",
-              
-              // Cho phép kết nối
-              "connect-src 'self' *.sanity.io *.facebook.com *.vercel.app chimpstatic.com embed.tawk.to",
-              
-              // Cho phép font
-              "font-src 'self' fonts.gstatic.com"
-            ].join('; ')
-          }
-        ],
-      },
-    ];
-  }
+  experimental: {
+    largePageDataBytes: 256 * 1000, // Tăng buffer size
+  },
+  // Cho phép bot Facebook truy cập
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        { 
+          key: 'Content-Security-Policy',
+          value: "default-src 'self' vercel.live cdn.sanity.io *.facebook.com *.fbcdn.net" 
+        },
+        { key: 'X-Frame-Options', value: 'ALLOW-FROM https://www.facebook.com' }
+      ],
+    }
+  ],
 }
 
 module.exports = nextConfig
