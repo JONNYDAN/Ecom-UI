d31b7713246ff80f4246a9f6c83d41b3f96b1430
JONNYDAN <tuannobi44@gmail.com>
2025-07-27 23:45:58 +0700

    updated new link

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -8,33 +8,12 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/product/:slug*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, max-age=0'
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOW-FROM https://www.facebook.com'
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://www.facebook.com"
-          }
-        ]
-      },
-      // Áp dụng cho toàn site
-      {
         source: '/:path*',
         headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          }
-        ]
-      }
-    ]
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+        ],
+      },
+    ];
   },
 }
 
